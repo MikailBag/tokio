50806315ea47bcdb0d2ffc087507b08b7bc1ee61
Mikail Bagishov <bagishov.mikail@yandex.ru>
2020-11-07 00:45:26 +0300

    1

--- tokio-util/src/sync/mod.rs
+++ tokio-util/src/sync/mod.rs
@@ -3,10 +3,5 @@
 mod mpsc;
 pub use mpsc::Sender;
 
-mod cancellation_token;
-pub use cancellation_token::{CancellationToken, WaitForCancellationFuture};
-
-mod intrusive_double_linked_list;
-
 #[cfg(test)]
 mod tests;
