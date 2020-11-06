cac4f8134de19684582306ce7c4341b2483ffa4b
Mikail Bagishov <bagishov.mikail@yandex.ru>
2020-11-07 00:40:20 +0300

    1

--- tokio-util/src/lib.rs
+++ tokio-util/src/lib.rs
@@ -1,27 +1,6 @@
-#![doc(html_root_url = "https://docs.rs/tokio-util/0.5.0")]
-#![allow(clippy::needless_doctest_main)]
-#![warn(
-    missing_debug_implementations,
-    missing_docs,
-    rust_2018_idioms,
-    unreachable_pub
-)]
 #![cfg_attr(docsrs, deny(broken_intra_doc_links))]
 #![doc(test(
     no_crate_inject,
     attr(deny(warnings, rust_2018_idioms), allow(dead_code, unused_variables))
 ))]
-#![cfg_attr(docsrs, feature(doc_cfg))]
-
-//! Utilities for working with Tokio.
-//!
-//! This crate is not versioned in lockstep with the core
-//! [`tokio`] crate. However, `tokio-util` _will_ respect Rust's
-//! semantic versioning policy, especially with regard to breaking changes.
-//!
-//! [`tokio`]: https://docs.rs/tokio
-
-mod loom;
-
-
 pub mod sync;
