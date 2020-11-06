fa59f6d94df33c6cdcf6df26d60bba497002dc7e
Mikail Bagishov <bagishov.mikail@yandex.ru>
2020-11-07 01:21:17 +0300

    1

--- tokio/src/sync/mpsc/mod.rs
+++ tokio/src/sync/mpsc/mod.rs
@@ -9,20 +9,10 @@ mod chan;
 
 pub(super) mod list;
 
-//mod unbounded;
-//pub use self::unbounded::{unbounded_channel, UnboundedReceiver, UnboundedSender};
-
 pub mod error;
 
 /// The number of values a block can contain.
 ///
 /// This value must be a power of 2. It also must be smaller than the number of
 /// bits in `usize`.
-#[cfg(all(target_pointer_width = "64", not(loom)))]
 const BLOCK_CAP: usize = 32;
-
-#[cfg(all(not(target_pointer_width = "64"), not(loom)))]
-const BLOCK_CAP: usize = 16;
-
-#[cfg(loom)]
-const BLOCK_CAP: usize = 2;
