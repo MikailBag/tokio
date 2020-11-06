c020bbdb23ad920c2530f9958dccbbad727c7dab
Mikail Bagishov <bagishov.mikail@yandex.ru>
2020-11-07 00:45:53 +0300

    1

--- tokio-util/src/lib.rs
+++ tokio-util/src/lib.rs
@@ -1,6 +1,1 @@
-#![cfg_attr(docsrs, deny(broken_intra_doc_links))]
-#![doc(test(
-    no_crate_inject,
-    attr(deny(warnings, rust_2018_idioms), allow(dead_code, unused_variables))
-))]
 pub mod sync;
